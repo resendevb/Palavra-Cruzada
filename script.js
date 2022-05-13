6dcd6e3fd62244b12660e82509702118b7adbbbf
Rafael <rafael.fuzeti@sptech.school>
2022-05-13 00:58:51 -0300

    js

---
+++ script.js
@@ -0,0 +1,187 @@
+function validar() {
+    var um_um = a_a.value        /* Q */
+    var um_dois = a_b.value      /* U */
+    var um_tres = a_c.value      /* A */
+    var um_quatro = a_d.value    /* D */
+    var um_cinco = a_e.value     /* C */
+    var um_seis = a_f.value      /* O */
+    var um_sete = a_g.value      /* R */
+    var um_oito = a_h.value      /* E */
+
+    if(um_um == "Q" && um_dois == "U" && um_tres == "A" && um_quatro == "D" && um_cinco == "C" && um_seis == "O" && um_sete == "R" && um_oito == "E" ) {
+
+        a_a.style.borderColor = 'green'
+        a_a.style.borderStyle = 'solid'
+        a_a.style.borderWidth = '5px'
+
+        a_b.style.borderColor = 'green'
+        a_b.style.borderStyle = 'solid'
+        a_b.style.borderWidth = '5px'
+
+        a_c.style.borderColor = 'green'
+        a_c.style.borderStyle = 'solid'
+        a_c.style.borderWidth = '5px'
+
+        a_d.style.borderColor = 'green'
+        a_d.style.borderStyle = 'solid'
+        a_d.style.borderWidth = '5px'
+
+        a_e.style.borderColor = 'green'
+        a_e.style.borderStyle = 'solid'
+        a_e.style.borderWidth = '5px'
+
+        a_f.style.borderColor = 'green'
+        a_f.style.borderStyle = 'solid'
+        a_f.style.borderWidth = '5px'
+
+        a_g.style.borderColor = 'green'
+        a_g.style.borderStyle = 'solid'
+        a_g.style.borderWidth = '5px'
+
+        a_h.style.borderColor = 'green'
+        a_h.style.borderStyle = 'solid'
+        a_h.style.borderWidth = '5px'
+
+       
+
+    }
+    else {
+        a_a.style.borderColor = 'none'
+    }
+
+
+    var dois_um = b_a.value     /* D */
+var dois_dois = b_b.value   /* M */
+/* ta na "quatro"  */       /* A */
+
+var tres_um = c_a.value     /* U */
+var tres_dois = c_b.value   /* L */
+/* ta na "um" */            /* A */
+
+/* ta na "um" */                /* D */
+var quatro_dois = d_b.value     /* U */
+var quatro_tres = d_c.value     /* A */
+var quatro_quatro = d_d.value   /* L */
+var quatro_cinco = d_e.value    /* C */
+var quatro_seis = d_f.value     /* O */
+var quatro_sete = d_g.value     /* R */
+var quatro_oito = d_h.value     /* E */
+
+/* ta na "quatro" */         /* C */
+var cinco_dois = e_b.value   /* S */
+
+/* ta na "quatro" */        /* R */
+var seis_dois = f_b.value   /* A */
+var seis_tres = f_c.value   /* M */
+
+var sete_um = g_a.value     /* R */
+var sete_dois = g_b.value   /* O */
+/* ta na "nove" */
+
+var oito_um = h_a.value      /* F */
+var oito_dois = h_b.value    /* L */
+/* ta na "nove" */           /* A */
+var oito_quatro = h_d.value  /* S */
+var oito_cinco = h_e.value   /* H */
+
+var nove_um = i_a.value        /* M */
+var nove_dois = i_b.value      /* E */
+var nove_tres = i_c.value      /* M */
+var nove_quatro = i_d.value    /* O */
+var nove_cinco = i_e.value     /* R */
+var nove_seis = i_f.value      /* I */
+var nove_sete = i_g.value      /* A */
+var nove_oito = i_h.value      /* D */
+var nove_nove = i_i.value      /* E */
+var nove_dez = i_j.value       /* M */
+var nove_onze = i_k.value      /* A */
+var nove_doze = i_l.value      /* S */
+var nove_treze = i_m.value     /* S */
+var nove_catorze = i_n.value   /* A */
+
+/* ta no "nove" */              /* A */
+var dez_dois = j_b.value        /* D */
+/* ta no onze */                /* R */
+var dez_quatro = j_d.value      /* E */
+/* ta no doze */                /* S */
+var dez_seis = j_f.value        /* S */
+var dez_sete = j_g.value        /* B */
+/* ta no "catorze" */           /* U */
+var dez_nove = j_i.value        /* S */
+
+var onze_um = k_a.value         /* E */
+var onze_dois = k_b.value       /* P */
+var onze_tres = k_c.value       /* R */
+var onze_quatro = k_d.value     /* O */
+var onze_cinco = k_e.value      /* M */
+
+var doze_um = l_a.value         /* I */
+var doze_dois = l_b.value       /* S */
+var doze_tres = l_c.value       /* E */
+var doze_quatro = l_d.value     /* T */
+var doze_cinco = l_e.value      /* E */
+
+var treze_um = m_a.value        /* I */
+var treze_dois = m_b.value      /* C */
+var treze_tres = m_c.value      /* I */
+var treze_quatro = m_d.value    /* N */
+var treze_cinco = m_e.value     /* C */
+/* ta no "quinze" */            /* O */
+
+/* ta no "treze" */             /* C */
+var catorze_um = n_b.value      /* P */
+var catorze_dois = n_c.value    /* U */
+
+var quinze_um = o_a.value           /* R */
+var quinze_dois = o_b.value         /* E */
+var quinze_tres = o_c.value         /* G */
+var quinze_quatro = o_d.value       /* I */
+var quinze_cinco = o_e.value        /* S */
+var quinze_seis = o_f.value         /* T */
+var quinze_sete = o_g.value         /* R */
+var quinze_oito = o_h.value         /* A */
+var quinze_nove = o_i.value         /* D */
+var quinze_dez = o_j.value          /* O */
+var quinze_onze = o_k.value         /* R */
+var quinze_doze = o_l.value         /* E */
+var quinze_treze = o_m.value        /* S */
+
+var dezesseis_um = p_a.value        /* D */
+/* ta no "quinze" */                /* A */
+var dezesseis_tres = p_c.value      /* T */
+var dezesseis_quatro = p_d.value    /* A */
+var dezesseis_cinco = p_e.value     /* B */
+var dezesseis_seis = p_f.value      /* U */
+var dezesseis_sete = p_g.value      /* S */
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  if(um_um == "Q"){
+     a_a.style.borderColor = 'green'
+     a_a.style.borderStyle = 'solid'
+     a_a.style.borderWidth = '5px'
+ }
+ else {
+     a_a.style.borderColor = 'red'
+ } */
